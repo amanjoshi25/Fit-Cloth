e283b64bd251b19545f6ef334e2d84021a36f5bb
Aman Joshi <92978854+amanjoshi25@users.noreply.github.com>
2024-06-06 12:14:15 +0530

    Add files via upload

--- homeScript.js
+++ homeScript.js
@@ -11,3 +11,36 @@ $(document).ready(function() {
         $('.menu-btn i').removeClass('active');
     });
 });
+// homeScript.js
+
+// homeScript.js
+
+$(document).ready(function(){
+    $('.designs-carousel').owlCarousel({
+      loop: true,
+      margin: 20,
+    //   nav: true,
+      dots:true,
+      autoplay: true, // Enable auto-play
+      autoplayTimeout: 3000, // Time between slides in milliseconds (3 seconds)
+      autoplayHoverPause: true, // Pause auto-play on hover
+      responsive: {
+        0: {
+          items: 1
+        
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
+        }
+      }
+    });
+  
+    // Optional: Add event listener to each card for a console log on click
+    $('.designs .card').on('click', function() {
+      console.log('Card clicked:', $(this).find('p').text());
+    });
+  });
+    
